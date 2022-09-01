143409a99aab43a3a7a4a0317e1cad8cc7e5b5bd
newsekaes <282695040@qq.com>
2022-09-02 01:37:21 +0800

    update: thisParameterType

--- utilty-type/thisParameterType.ts
+++ utilty-type/thisParameterType.ts
@@ -4,7 +4,9 @@ type Fn = (this: number) => void
 const thisParam: ThisParameterType<Fn> = 6
 
 /*  ============== write code ============= */
-export type MyThisParameterType<T> = T extends (this: infer K, ...args: any[]) => any ? K : unknown
+/* https://stackoverflow.com/questions/67713566/how-does-the-never-type-work-in-typescript */
+/* 协变和逆变 https://en.wikipedia.org/wiki/Covariance_and_contravariance_(computer_science) */
+export type MyThisParameterType<T> = T extends (this: infer K, ...args: never) => any ? K : unknown
 
 /*  ============== test ============= */
 
