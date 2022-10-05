ada8976797c0af7839432b0847b61e1e829df3fe
newsekaes <282695040@qq.com>
2022-10-05 23:34:32 +0800

    feat: TupleToNestedObject

---
+++ type-challenges/medium/TupleToNestedObject.ts
@@ -0,0 +1,15 @@
+type TupleToNestedObject<T, U> =
+  T extends [infer First, ...infer Rest]
+    ? { [P in First & string]: TupleToNestedObject<Rest, U> }
+    : U
+
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<TupleToNestedObject<['a'], string>, { a: string }>>,
+  Expect<Equal<TupleToNestedObject<['a', 'b'], number>, { a: { b: number } }>>,
+  Expect<Equal<TupleToNestedObject<['a', 'b', 'c'], boolean>, { a: { b: { c: boolean } } }>>,
+  Expect<Equal<TupleToNestedObject<[], boolean>, boolean>>,
+]
