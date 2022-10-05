5f5e720f717e574e17fc9a563d82623e2615306f
newsekaes <282695040@qq.com>
2022-10-05 23:10:45 +0800

    feat: Shift

---
+++ type-challenges/medium/Shift.ts
@@ -0,0 +1,10 @@
+type Shift<T> = T extends [infer First, ...infer Rest] ? Rest : T
+
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Shift<[3, 2, 1]>, [2, 1]>>,
+  Expect<Equal<Shift<['a', 'b', 'c', 'd']>, ['b', 'c', 'd']>>,
+]
