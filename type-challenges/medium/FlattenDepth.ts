bbbe68176e13fe0d877ac143c907eebd66be5bf7
newsekaes <282695040@qq.com>
2022-10-08 23:39:44 +0800

    feat: FlattenDepth

---
+++ type-challenges/medium/FlattenDepth.ts
@@ -0,0 +1,21 @@
+type FlattenDepth<T, K = 1, Counter extends unknown[] = []> = Counter["length"] extends K
+  ? T 
+  : T extends [infer First, ...infer Rest]
+    ? First extends any[]
+      ? [...FlattenDepth<First, K, [...Counter, 0]>, ...FlattenDepth<Rest, K, Counter>]
+      : [First, ...FlattenDepth<Rest, K , Counter>]
+    : T
+
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<FlattenDepth<[]>, []>>,
+  Expect<Equal<FlattenDepth<[1, 2, 3, 4]>, [1, 2, 3, 4]>>,
+  Expect<Equal<FlattenDepth<[1, [2]]>, [1, 2]>>,
+  Expect<Equal<FlattenDepth<[1, 2, [3, 4], [[[5]]]], 2>, [1, 2, 3, 4, [5]]>>,
+  Expect<Equal<FlattenDepth<[1, 2, [3, 4], [[[5]]]]>, [1, 2, 3, 4, [[5]]]>>,
+  Expect<Equal<FlattenDepth<[1, [2, [3, [4, [5]]]]], 3>, [1, 2, 3, 4, [5]]>>,
+  Expect<Equal<FlattenDepth<[1, [2, [3, [4, [5]]]]], 19260817>, [1, 2, 3, 4, 5]>>,
+]
