07ac7dbd03898adc32b86b667b28c02a5675716a
newsekaes <282695040@qq.com>
2022-10-19 23:32:18 +0800

    feat: TrimRight

---
+++ type-challenges/medium/TrimRight.ts
@@ -0,0 +1,17 @@
+type Whitespace = " " | "\n" | '\t'
+
+type TrimRight<S extends string> = S extends `${infer Rest}${Whitespace}` ? TrimRight<Rest> : S
+
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<TrimRight<'str'>, 'str'>>,
+  Expect<Equal<TrimRight<'str '>, 'str'>>,
+  Expect<Equal<TrimRight<'str     '>, 'str'>>,
+  Expect<Equal<TrimRight<'     str     '>, '     str'>>,
+  Expect<Equal<TrimRight<'   foo bar  \n\t '>, '   foo bar'>>,
+  Expect<Equal<TrimRight<''>, ''>>,
+  Expect<Equal<TrimRight<'\n\t '>, ''>>,
+]
