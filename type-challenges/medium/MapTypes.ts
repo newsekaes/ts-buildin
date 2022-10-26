62223840ca83461f07c164a60fbb76ef7a1ccf2d
newsekaes <282695040@qq.com>
2022-10-27 00:48:06 +0800

    feat(challange): MapTypes

---
+++ type-challenges/medium/MapTypes.ts
@@ -0,0 +1,24 @@
+/* _____________ Your Code Here _____________ */
+type MapTypes<T, R extends Record<"mapFrom" | "mapTo", any>> = {
+  [P in keyof T]: T[P] extends R["mapFrom"] 
+    ? R extends R
+      ? Equal<T[P], R["mapFrom"]> extends true
+        ? R["mapTo"]
+        : never
+      : never
+    : T[P]
+}
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<MapTypes<{ stringToArray: string }, { mapFrom: string; mapTo: [] }>, { stringToArray: [] }>>,
+  Expect<Equal<MapTypes<{ stringToNumber: string }, { mapFrom: string; mapTo: number }>, { stringToNumber: number }>>,
+  Expect<Equal<MapTypes<{ stringToNumber: string; skipParsingMe: boolean }, { mapFrom: string; mapTo: number }>, { stringToNumber: number; skipParsingMe: boolean }>>,
+  Expect<Equal<MapTypes<{ date: string }, { mapFrom: string; mapTo: Date } | { mapFrom: string; mapTo: null }>, { date: null | Date }>>,
+  Expect<Equal<MapTypes<{ date: string }, { mapFrom: string; mapTo: Date | null }>, { date: null | Date }>>,
+  Expect<Equal<MapTypes<{ fields: Record<string, boolean> }, { mapFrom: Record<string, boolean>; mapTo: string[] }>, { fields: string[] }>>,
+  Expect<Equal<MapTypes<{ name: string }, { mapFrom: boolean; mapTo: never }>, { name: string }>>,
+  Expect<Equal<MapTypes<{ name: string; date: Date }, { mapFrom: string; mapTo: boolean } | { mapFrom: Date; mapTo: string }>, { name: boolean; date: string }>>,
+]
